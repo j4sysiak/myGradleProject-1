2a9247891fafeff9be818604a78aba260a6f6a39
j4sysiak <j4sysiak@gmail.com>
2018-11-24 08:41:39 +0100

    1

---
+++ myGradleProject-1/src/main/java/pl/jaceksysiak/main/App.java
@@ -0,0 +1,13 @@
+package pl.jaceksysiak.main;
+
+ import java.util.Arrays;
+ import org.apache.commons.lang3.StringUtils;
+
+public class App {
+
+	public static void main(String[] args) {
+		 
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+	}
+
+}
